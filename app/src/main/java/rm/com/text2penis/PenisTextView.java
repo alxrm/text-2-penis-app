80c01f53d169762ec2e6cd76a6290a2bdccdb9a2
alex <rupta3@gmail.com>
2017-04-12 12:08:37 +0300

    Initial commit

---
+++ app/src/main/java/rm/com/text2penis/PenisTextView.java
@@ -0,0 +1,26 @@
+package rm.com.text2penis;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by alex
+ */
+public final class PenisTextView extends View {
+
+  public PenisTextView(Context context) {
+    super(context);
+  }
+
+  public PenisTextView(Context context, @Nullable AttributeSet attrs) {
+    super(context, attrs);
+  }
+
+  public PenisTextView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+    super(context, attrs, defStyleAttr);
+  }
+
+
+}
