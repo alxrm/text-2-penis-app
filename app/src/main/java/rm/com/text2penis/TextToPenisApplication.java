dd8d35606e31f91c35f629611790ec5a962b8e1b
alex <rupta3@gmail.com>
2017-04-13 13:38:35 +0300

    Added analytics, updated deps versions

--- app/src/main/java/rm/com/text2penis/TextToPenisApplication.java
+++ app/src/main/java/rm/com/text2penis/TextToPenisApplication.java
@@ -1,6 +1,9 @@
 package rm.com.text2penis;
 
 import android.app.Application;
+import com.crashlytics.android.Crashlytics;
+import com.crashlytics.android.answers.Answers;
+import io.fabric.sdk.android.Fabric;
 
 /**
  * Created by alex
@@ -8,7 +11,6 @@ import android.app.Application;
 public final class TextToPenisApplication extends Application {
   @Override public void onCreate() {
     super.onCreate();
-
-    // TODO add analytics here
+    Fabric.with(this, new Crashlytics(), new Answers());
   }
 }
